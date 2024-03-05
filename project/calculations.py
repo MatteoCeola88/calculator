42e45d83c5adba5ea64d4c7ddd188692f87b2f8d
MatteoCeola88 <matteo.ceola.1@studenti.unipd.it>
2024-03-05 11:53:30 +0100

    Add project code and documentation

---
+++ project/calculations.py
@@ -0,0 +1,19 @@
+# calculator/calculations.py
+
+def add(a, b):
+    """Computes and  Returns the sum of `a` and `b`.
+
+    Examples:
+        >>> add(1, 8)
+        9.0
+        >>> add(3, 4)
+        7.0
+
+    Args:
+        a (float): The first addend.
+        b (float): The second addend.
+
+    Returns:
+        float:The sum of the two addends `a` and `b`.
+     """
+    return float(a+b)
